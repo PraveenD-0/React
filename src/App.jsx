93f9b81223a2dd258d5193653bd6503f210f5b1c
Praveen-D-0 <jopraveen06@gmail.com>
2023-11-28 21:25:24 +0530

    traditional page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
- const App = () => {
-    return (
-        <p> Hello World</p>
-    )
+import React, { useState } from 'react'
+import Home from './components/Home';
+import Notes from './components/Notes';
+
+function App() {
+  const [page, setPage] = useState('home');
+
+  const toPage = (page) => (event) => {
+    event.preventDefault();
+    setPage(page);
+  }
+
+  const content = () => {
+    if (page === 'home') {
+      return <Home />
+    } else if (page === 'notes') {
+      return <Notes />
+    } else if (page === 'users') {
+      return <Users />
     }
-    
-    export default App;
+  }
+  const padding = {
+    padding: 5,
+  }
+  return (
+    <div>
+       <div>
+        <a href='' style={padding} onClick={toPage('home')}>home</a>
+        <a href='' style={padding} onClick={toPage('notes')}>notes</a>
+        <a href='' style={padding} onClick={toPage('users')}>users</a>
+       </div>
+       {/* render the content of the component for the current set page*/}
+       { content ()}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Users.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ function Users() {
+   return (
+     <div>
+       <P>Users</P>
+     </div>
+   )
+ }
+ 
+ export default Users;
+ 
