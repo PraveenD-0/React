0475823aa8fb1188f8c0e7ea1d53e376788e0642
Praveen-D-0 <jopraveen06@gmail.com>
2023-11-29 21:33:54 +0530

    button background bodycolor switcher with useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import { useRef } from 'react';
 
 function App() {
-  const buttonRef = useRef(null);
+  // const buttonRef = useRef(null);
+  const bodyRef = useRef(document.body);
 
   const changeColor = () => {
+
     // console.log('chang color button clicked')
     // console.log(buttonRef.current);
     const colors = ['red','blue', 'green', 'yellow','purple','pink','black','orange','brown','white'];
     const randomColor = colors[Math.floor(Math.random()*10)];
-    console.log(randomColor);
-    buttonRef.current.style.backgroundColor = randomColor;
+     
+    bodyRef.current.style.backgroundColor = randomColor;
   }
   return (
     <div>
       <h3>Background Color Switcher</h3>
-      <button onClick={changeColor} ref={buttonRef}>Change Color</button>
+      <button onClick={changeColor}>Change Color</button>
     </div>
   )
 }
