af6255babed3715edfcda0ffab58573c94fba6cf
Praveen-D-0 <jopraveen06@gmail.com>
2023-11-29 21:11:14 +0530

    placing input reference inside useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 
- import React, { useRef } from 'react';
+ import React, { useEffect, useRef } from 'react';
  
  function App() {
   
-
-   const inputRef = useRef(null);
-   const handleButtonClick = () => {
-    // console.log(inputRef.current.value);
-    // inputRef.current.value='praveen';
+  const inputRef = useRef(null);
+  useEffect(() => {
     inputRef.current.focus();
-   }
+    }, []);
+  
    return (
      <div>
        <input
        type='text'
        placeholder='type a note...'
        ref={inputRef}
-
       />
-      <button onClick={handleButtonClick}>Focus Input</button>
      </div>
    )
  }
