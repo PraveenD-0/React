51f9870df0c7cfe3f362fb4c2810dcdde1982f40
Praveen-D-0 <jopraveen06@gmail.com>
2023-12-10 12:18:16 +0530

    added dashboard content

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,77 @@
-import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-import Dashboard from './components/Dashboard';
+import React, { useEffect, useRef, useState } from 'react';
 import ReadNotes from './components/ReadNotes';
 import CreateNote from './components/CreateNote';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
 
- 
 function App() {
-
-  const [showStatus, setShowStatus] = useState('all');
   const [notes, setNotes] = useState([]);
+  const [showStatus, setShowStatus] = useState('all');
   const [newNoteContent, setNewNoteContent] = useState('');
   const [newNoteImportant, setNewNoteImportant] = useState('true');
   const newNoteContentRef = useRef(null);
-  
+
   const fetchNotes = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/notes');
+      const response = await axios.get('http://localhost:3000/notes/');
+      // console.log(response.data);
       setNotes(response.data);
     } catch (error) {
       console.log('Failed to fetch notes:', error);
     }
   } 
-  useEffect(() => {
-    newNoteContentRef.current.focus();
-  }, []);
 
   useEffect(() => {
     fetchNotes();
   }, []); 
+
   const addNote = (event) => {
     event.preventDefault();
-    
+
     let noteObject = {
       id: notes.length + 1,
       content: newNoteContent,
-      important: newNoteImportant === 'true',
+      important: newNoteImportant == 'true',
     }
 
-    // setNotes(notes.concat(noteObject));
-
     console.log('adding a new note...');
-    axios.post('http://localhost:3000/notes',
-     noteObject)
-     .then(response => {
-      console.log('note added successfully...')
-     })
-  
+    axios
+      .post('http://localhost:3000/notes/', noteObject)
+      .then(response => {
+        console.log('note added successfully...');
+      })
+
     setNewNoteContent('');
     setNewNoteImportant('');
 
     newNoteContentRef.current.focus();
+
     fetchNotes();
   }
+
   const handleStatusChange = (event) => {
     setShowStatus(event.target.value);
   }
 
+  const padding = {
+    paddingRight: 15,
+  }
+
   return (
-    <div>
-      <ReadNotes showStatus={ showStatus } 
-        handleStatusChange={ handleStatusChange }
-        notes={ notes }
-      />
-      <hr></hr>
-      <CreateNote addNote={addNote} 
-        newNoteContent={newNoteContent}
-        newNoteImportant={newNoteImportant}
-        newNoteContentRef= {newNoteContentRef}
-        setNewNoteContent={setNewNoteContent}
-        setNewNoteImportant={setNewNoteImportant}
-      />
-    </div>
+    <Router>
+      <div>
+        <Link to="/" style={padding}>Dashboard</Link>
+        <Link to="/read" style={padding}>Read Notes</Link>
+        <Link to="/create" style={padding}>Create Note</Link>
+      </div>
+
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/read' element={<ReadNotes showStatus={showStatus} handleStatusChange={handleStatusChange} notes={notes} />} />
+        <Route path='/create' element={<CreateNote addNote={addNote} newNoteContent={newNoteContent} newNoteImportant={newNoteImportant} newNoteContentRef={newNoteContentRef} setNewNoteContent={setNewNoteContent} setNewNoteImportant={ setNewNoteImportant} />} />
+      </Routes>
+    </Router>
   )
 }
 
