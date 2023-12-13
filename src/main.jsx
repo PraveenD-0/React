89fd66ed17251739c4db3b1cf0867dfa9f0a55fa
Praveen-D-0 <jopraveen06@gmail.com>
2023-12-13 13:17:46 +0530

    simple redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,42 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { createStore } from 'redux';
 
 
+const counterReducer = (state = 0, action) => {
+    console.log(action);
+    switch (action.type) {
+        case 'INCR':
+            return state + 1;
+        case 'DECR':
+            return state - 1;
+        default:
+            return state;
+    }
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App  />);
+const store = createStore(counterReducer);
+
+
+console.log(store.getState());
+
+store.dispatch({
+    type: 'INCR'
+})
+
+console.log(store.getState());
+
+store.dispatch({
+    type: 'INCR'
+})
+
+console.log(store.getState());
+
+store.dispatch({
+    type: 'DECR'
+})
+
+console.log(store.getState());
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
