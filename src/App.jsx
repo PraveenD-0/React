efb6ccda1317ca624cd1cde1f54d29328979cd3e
Praveen-D-0 <jopraveen06@gmail.com>
2023-12-13 16:11:28 +0530

    useref -input element

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,31 @@
-import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
-import ReadNotes from './components/ReadNotes';
-import CreateNote from './components/CreateNote';
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import EditNote from './components/EditNote';
-import DeleteNote from './components/DeleteNote'; 
+
+// useRef() definition: useRef returns a mutable ref object whose .current property is initialized to the passed argument (initialValue). The returned object will persist for the full lifetime of the component.
+
+
+import React, { useRef } from 'react';
 
 function App() {
-  const [notes, setNotes] = useState([]);
-  const [showStatus, setShowStatus] = useState('all');
-  const [newNoteContent, setNewNoteContent] = useState('');
-  const [newNoteImportant, setNewNoteImportant] = useState('true');
-  const newNoteContentRef = useRef(null);
 
-  const fetchNotes = async () => {
-    try {
-      const response = await axios.get('http://localhost:3000/notes/');
-      // console.log(response.data);
-      setNotes(response.data);
-    } catch (error) {
-      console.log('Failed to fetch notes:', error);
-    }
-  } 
+  const inputRef = useRef(null);
 
-  useEffect(() => {
-    fetchNotes();
-  }, []); 
+  // print the inputRef current value
+  // inputRef -> {current: input} object
+  // inputRef.current -> input DOM node (element)
+  // inputRef.current.value -> input DOM node value
+  console.log(inputRef.current);
 
-  const addNote = (event) => {
-    event.preventDefault();
-
-    let noteObject = {
-      id: notes.length + 1,
-      content: newNoteContent,
-      important: newNoteImportant == 'true',
-    }
-
-    console.log('adding a new note...');
-    axios
-      .post('http://localhost:3000/notes/', noteObject)
-      .then(response => {
-        console.log('note added successfully...');
-      })
-
-    setNewNoteContent('');
-    setNewNoteImportant('');
-
-    newNoteContentRef.current.focus();
-
-    fetchNotes();
-  }
-
-  const handleStatusChange = (event) => {
-    setShowStatus(event.target.value);
-  }
-
-  const padding = {
-    paddingRight: 15,
+  const handleInputChange = () => {
+    console.log(inputRef.current.value);
   }
 
   return (
-    <Router>
-      <div>
-        <Link to="/" style={padding}>Dashboard</Link>
-        <Link to="/read" style={padding}>Read Notes</Link>
-        <Link to="/create" style={padding}>Create Note</Link>
-        <Link to='/update' style={padding}>update Note</Link>
-        <Link to='/delete' style={padding}>Delete Note</Link>
-      </div>
-
-      <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/read' element={<ReadNotes showStatus={showStatus} handleStatusChange={handleStatusChange} notes={notes} />} />
-        <Route path='/create' element={<CreateNote addNote={addNote} newNoteContent={newNoteContent} newNoteImportant={newNoteImportant} newNoteContentRef={newNoteContentRef} setNewNoteContent={setNewNoteContent} setNewNoteImportant={ setNewNoteImportant} />} />
-        <Route path='/update' element={ <EditNote notes={notes} setNotes={ setNotes } fetchNotes={fetchNotes} />} /> 
-        <Route path='/delete' element={ <DeleteNote notes={notes} setNotes={setNotes} /> } />
-      </Routes>
-    </Router>
+    <div>
+      <input
+        type='text'
+        ref={inputRef}
+        onChange={handleInputChange}
+      ></input>
+    </div>
   )
 }
 
